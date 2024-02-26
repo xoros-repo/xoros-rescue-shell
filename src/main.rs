ce70a26d6ff204ff4c0e2773ff934a2916034f72
amochkin <a@mochkin.com>
2024-02-26 17:41:23 +0100

    fix: no matching package named `cursive` found

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,71 @@
 use cursive::{event::Key, menu, traits::*, views::Dialog};
 use std::sync::atomic::{AtomicUsize, Ordering};
-use cursive::theme::{Color, Palette, PaletteColor, Theme};
+use cursive::theme::{BaseColor, Color, Palette, PaletteColor, Theme};
+
+pub trait StatusBarExt {
+    fn status_bar(&mut self, content: impl Into<cursive::utils::span::SpannedString<cursive::theme::Style>>) -> cursive::views::TextContent;
+    fn get_status_bar_content(&mut self) -> cursive::views::TextContentRef;
+    fn set_status_bar_content(&mut self, content: impl Into<cursive::utils::span::SpannedString<cursive::theme::Style>>);
+}
+
+impl StatusBarExt for cursive::Cursive {
+    /// Create a new status bar, set to the given content.
+    fn status_bar(&mut self, content: impl Into<cursive::utils::span::SpannedString<cursive::theme::Style>>) -> cursive::views::TextContent {
+        let text_content = cursive::views::TextContent::new(content);
+        self.screen_mut()
+            .add_transparent_layer(
+                cursive::views::OnLayoutView::new(
+                    cursive::views::FixedLayout::new().child(
+                        cursive::Rect::from_point(cursive::Vec2::zero()),
+                        cursive::views::Layer::new(
+                            cursive::views::TextView::new_with_content(text_content.clone()).with_name("status"),
+                        )
+                            .full_width(),
+                    ),
+                    |layout, size| {
+                        let rect = cursive::Rect::from_size((0, size.y - 1), (size.x, 1));
+                        layout.set_child_position(0, rect);
+                        layout.layout(size);
+                    },
+                )
+                    .full_screen(),
+            );
+        text_content
+    }
+
+    fn get_status_bar_content(&mut self) -> cursive::views::TextContentRef {
+        self.call_on_name("status", |text_view: &mut cursive::views::TextView| {
+            text_view.get_content()
+        })
+            .expect("get_status")
+    }
+
+    fn set_status_bar_content(&mut self, content: impl Into<cursive::utils::span::SpannedString<cursive::theme::Style>>) {
+        self.call_on_name("status", |text_view: &mut cursive::views::TextView| {
+            text_view.set_content(content);
+        })
+            .expect("set_status")
+    }
+
+}
 
 fn main() {
-    let mut siv = cursive::default();
+    let mut c = cursive::termion();
     let mut theme = Theme::default();
     let mut palette = Palette::default();
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-    palette[PaletteColor::Background] = Color::Rgb(30, 30 ,30);
+    palette[PaletteColor::Background] = Color::Light(BaseColor::Black);
+    palette[PaletteColor::View] = Color::Light(BaseColor::Yellow);
+    palette[PaletteColor::Highlight] = Color::Dark(BaseColor::Black);
+    palette[PaletteColor::HighlightText] = Color::Light(BaseColor::White);
     theme.palette = palette;
-    siv.set_theme(theme);
+    c.set_theme(theme);
 
     // We'll use a counter to name new files.
     let counter = AtomicUsize::new(1);
 
-    siv.menubar()
+    c.menubar()
         .add_subtree(
             "File",
             menu::Tree::new()
@@ -70,7 +121,7 @@ fn main() {
                         }),
                 )
                 .leaf("About", |s| {
-                    s.add_layer(Dialog::info("Xoros Rescue Shell v0.0.0"))
+                    s.add_layer(Dialog::info(format!("Xoros Rescue Shell v{}", VERSION)))
                 }),
         )
         .add_delimiter()
@@ -82,9 +133,9 @@ fn main() {
 
 //    siv.set_autohide_menu(false);
 
-    siv.add_global_callback(Key::Esc, |s| s.select_menubar());
+    c.add_global_callback(Key::Esc, |s| s.select_menubar());
 
-    siv.add_layer(Dialog::text("Xoros Rescue Shell\nPress ESC to continue..."));
-
-    siv.run();
+    c.add_layer(Dialog::text("Press ESC to continue..."));
+    c.status_bar(format!("Xoros Rescue Shell v{}", VERSION));
+    c.run();
 }
