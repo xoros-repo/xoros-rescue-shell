56d8a66bd4bca2983463dd57144dbf28265ab0b3
amochkin <a@mochkin.com>
2024-02-26 10:38:51 +0100

    fix: no matching package named `cursive` found

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,17 @@
 use cursive::{event::Key, menu, traits::*, views::Dialog};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-// This examples shows how to configure and use a menubar at the top of the
-// application.
-
 fn main() {
     let mut siv = cursive::default();
 
     // We'll use a counter to name new files.
     let counter = AtomicUsize::new(1);
 
-    // The menubar is a list of (label, menu tree) pairs.
     siv.menubar()
-        // We add a new "File" tree
         .add_subtree(
             "File",
             menu::Tree::new()
-                // Trees are made of leaves, with are directly actionable...
                 .leaf("New", move |s| {
-                    // Here we use the counter to add an entry
-                    // in the list of "Recent" items.
                     let i = counter.fetch_add(1, Ordering::Relaxed);
                     let filename = format!("New {i}");
                     s.menubar()
@@ -71,7 +63,7 @@ fn main() {
                         }),
                 )
                 .leaf("About", |s| {
-                    s.add_layer(Dialog::info("Cursive v0.0.0"))
+                    s.add_layer(Dialog::info("Xoros Rescue Shell v0.0.0"))
                 }),
         )
         .add_delimiter()
@@ -81,11 +73,11 @@ fn main() {
     // Turning it off will leave the menu always visible.
     // Try uncommenting this line!
 
-    // siv.set_autohide_menu(false);
+//    siv.set_autohide_menu(false);
 
     siv.add_global_callback(Key::Esc, |s| s.select_menubar());
 
-    siv.add_layer(Dialog::text("Hit <Esc> to show the menu!"));
+    siv.add_layer(Dialog::text("Xoros Rescue Shell\nPress ESC to continue..."));
 
     siv.run();
 }
