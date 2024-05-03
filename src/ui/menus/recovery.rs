76a1a17b1562b9540624da14c46ffc0ad6eafec9
amochkin <amochkin@gmail.com>
2024-05-03 12:23:46 +0200

    added recovery shell

--- src/ui/menus/recovery.rs
+++ src/ui/menus/recovery.rs
@@ -1,6 +1,7 @@
-use cursive::menu;
+use cursive::{CursiveExt, menu};
 use cursive::menu::Tree;
 use cursive::views::Dialog;
+use subprocess::Exec;
 
 pub fn recovery_menu() -> Tree {
     let tree = menu::Tree::new()
@@ -9,6 +10,18 @@ pub fn recovery_menu() -> Tree {
         })
         .leaf("Verify ROOT partitions", move |s| {
             s.add_layer(Dialog::info("Config was restored!"));
+        })
+        .leaf("Subshell", move |s| {
+            let state = s.dump();
+
+            let exit_status = Exec::shell("/bin/sh")
+                .detached()  // This detaches the shell from the parent process
+                .join()      // Wait for the shell to finish
+                .expect("Failed to launch shell");
+
+            s.restore(state);
+
+            s.add_layer(Dialog::info(format!("Shell exited with status: {:?}", exit_status)));
         })
         .delimiter()
         .leaf("Complete Reset", move |s| {
